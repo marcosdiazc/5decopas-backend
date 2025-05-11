5f0fe91dc982cadec83a5d403d4aca838f75153f
Tu Nombre <marcossaiddiazc@gmail.com>
2025-05-11 02:01:02 -0300

    add service changes

--- services/supabase_service.py
+++ services/supabase_service.py
@@ -6,6 +6,12 @@ supabase = create_client(
     os.getenv("SUPABASE_KEY")
 )
 
+def insertar_jugador(data):
+    return supabase.table("players").insert(data).execute()
+
+def obtener_jugadores():
+    return supabase.table("players").select("*").execute().data
+
 def obtener_jugador_por_id(id):
     res = supabase.table("players").select("*").eq("id", id).single().execute()
     return res.data
